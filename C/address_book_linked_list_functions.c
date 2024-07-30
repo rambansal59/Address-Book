e05e6ea5b6ab6339fe97d77f60ca0c5e648d1297
rambansal59 <134187413+rambansal59@users.noreply.github.com>
2024-07-30 08:33:43 +0530

    Update address_book_linked_list_functions.c

--- C/address_book_linked_list_functions.c
+++ C/address_book_linked_list_functions.c
@@ -1,10 +1,4 @@
 /********************
- * <copyright information>
- * AUTHOR: Michael (Micky) Mangrobang
- * COURSE: ICS 212 Program Structure
- * INSTRUCTOR: Ravi Narayan
- * DATE CREATED: 17 October 2015
- * REVISIONS: 07 June 2018
  * FILE: address_book_linked_list_functions.c
  * DESC: Source file that contains functions for the linked list
  ********************/
